c1d0b1a02c2d6bc802451aa221861b9bcbb4fc5c
say-ilyas <102316165+say-ilyas@users.noreply.github.com>
2022-05-24 20:02:30 +0300

    Initial commit

---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Timer.java
@@ -0,0 +1,16 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier ("animalCage")
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
